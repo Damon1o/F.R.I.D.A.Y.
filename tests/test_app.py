74c01a5e89fca6f1c721c083a92ec21fb8237137
Damon Lin <damonlin79@gmail.com>
2026-07-21 23:31:24 -0400

    chore: remove deprecated project files and configuration source code

--- tests/test_app.py
+++
@@ -1,3 +0,0 @@
-def test_app_boots(client):
-    response = client.get("/calendar")
-    assert response.status_code == 200
--- tests/test_dashboard.py
+++
@@ -1,8 +0,0 @@
-def test_dashboard_page_renders_cards_and_chat(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    body = response.data.decode()
-    assert "Planned Absences" in body
-    assert "Future Events" in body
-    assert "Onboarding" in body
-    assert 'id="dash-chat"' in body
